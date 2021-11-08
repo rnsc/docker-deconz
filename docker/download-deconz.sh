5ca0618591e5b35d5ef77b6312bdce6fe027506a
rnsc <github@rnsc.be>
2021-11-08 18:43:51 +0100

    builx env vars

--- docker/download-deconz.sh
+++ docker/download-deconz.sh
@@ -4,18 +4,17 @@ set -ex
 
 DECONZ_VERSION=$1
 CHANNEL=$2
+PLATFORM=$3
 
-uname -a
-
-if uname -a | grep -qE "arm64|aarch64";
+if echo "${PLATFORM}" | grep -qE "arm64|aarch64";
 then
   URL="http://deconz.dresden-elektronik.de/debian/${CHANNEL}/deconz_${DECONZ_VERSION}-debian-stretch-${CHANNEL}_arm64.deb"
 fi
-if uname -a | grep -qE "amd64";
+if echo "${PLATFORM}" | grep -qE "amd64";
 then
   URL="http://deconz.dresden-elektronik.de/ubuntu/${CHANNEL}/deconz-${DECONZ_VERSION}-qt5.deb"
 fi
-if uname -a | grep -qE "armhf|armv7";
+if echo "${PLATFORM}" | grep -qE "armhf|armv7";
 then
   URL="http://deconz.dresden-elektronik.de/raspbian/${CHANNEL}/deconz-${DECONZ_VERSION}-qt5.deb"
 fi
