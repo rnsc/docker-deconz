a198bfa78ccfd541b2a4d30b1750eff47e2458f8
rnsc <github@rnsc.be>
2021-11-08 18:53:28 +0100

    fix arm/v7 condition

--- docker/download-deconz.sh
+++ docker/download-deconz.sh
@@ -6,7 +6,7 @@ DECONZ_VERSION=$1
 CHANNEL=$2
 PLATFORM=$3
 
-if echo "${PLATFORM}" | grep -qE "arm64|aarch64";
+if echo "${PLATFORM}" | grep -qE "arm64";
 then
   URL="http://deconz.dresden-elektronik.de/debian/${CHANNEL}/deconz_${DECONZ_VERSION}-debian-stretch-${CHANNEL}_arm64.deb"
 fi
@@ -14,7 +14,7 @@ if echo "${PLATFORM}" | grep -qE "amd64";
 then
   URL="http://deconz.dresden-elektronik.de/ubuntu/${CHANNEL}/deconz-${DECONZ_VERSION}-qt5.deb"
 fi
-if echo "${PLATFORM}" | grep -qE "armhf|armv7";
+if echo "${PLATFORM}" | grep -qE "v7";
 then
   URL="http://deconz.dresden-elektronik.de/raspbian/${CHANNEL}/deconz-${DECONZ_VERSION}-qt5.deb"
 fi
