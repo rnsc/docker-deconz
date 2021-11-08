95c393ef9307068b0b87776caf1ff7c34287c572
rnsc <github@rnsc.be>
2021-11-08 18:36:13 +0100

    debug bash

--- docker/download-deconz.sh
+++ docker/download-deconz.sh
@@ -5,15 +5,17 @@ set -ex
 DECONZ_VERSION=$1
 CHANNEL=$2
 
-if uname -a | grep -Eq ".+(arm64|aarch64).+";
+uname -a
+
+if uname -a | grep -qE "arm64|aarch64";
 then
   URL="http://deconz.dresden-elektronik.de/debian/${CHANNEL}/deconz_${DECONZ_VERSION}-debian-stretch-${CHANNEL}_arm64.deb"
 fi
-if uname -a | grep -Eq ".+amd64.+";
+if uname -a | grep -qE "amd64";
 then
   URL="http://deconz.dresden-elektronik.de/ubuntu/${CHANNEL}/deconz-${DECONZ_VERSION}-qt5.deb"
 fi
-if uname -a | grep -q ".+(armhf|armv7).+";
+if uname -a | grep -qE "armhf|armv7";
 then
   URL="http://deconz.dresden-elektronik.de/raspbian/${CHANNEL}/deconz-${DECONZ_VERSION}-qt5.deb"
 fi
